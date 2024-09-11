e81b26e21221f4b52f648ddb45c86a0c873b4b7c
Akshat Saini <axshat.ind@gmail.com>
2024-09-12 00:30:01 +0530

    basic sign in card setup

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,3 +1,47 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+
 export const SignInCard = () => {
-  return <div>Sign In Card</div>;
+  return (
+    <Card className="w-full h-full p-8">
+      <CardHeader className="px-0 pt-0">
+        <CardTitle>Login to Continue</CardTitle>
+        <CardDescription>
+          Use you email or another service to continue
+        </CardDescription>
+      </CardHeader>
+
+      <CardContent className="space-y-5 px-0 pb-0">
+        {/* space y means elements inside that element will be placed out in y at 5 */}
+        <form className="space-y-2.5">
+          <Input
+            disabled={false}
+            value=""
+            onChange={() => {}}
+            placeholder="Email"
+            type="email"
+            required
+          />
+          <Input
+            disabled={false}
+            value=""
+            onChange={() => {}}
+            placeholder="Password"
+            type="password"
+            required
+          />
+          <Button type="submit" className="w-full" size="lg" disabled={false}>
+            Continue
+          </Button>
+        </form>
+      </CardContent>
+    </Card>
+  );
 };
