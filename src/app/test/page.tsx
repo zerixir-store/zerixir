1a9c9ca0439c6b602a320badbff8c193395d1615
Akshat Saini <axshat.ind@gmail.com>
2024-09-14 01:47:30 +0530

    auth screen at all urls,{1,1,48}

---
+++ src/app/test/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Test
+    </main>
+  );
+};
+
+export default Page;
