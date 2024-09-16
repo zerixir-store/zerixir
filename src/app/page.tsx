7e2feb279b11d89597bb550e965d8485c84a7cf5
Akshat Saini <axshat.ind@gmail.com>
2024-09-17 00:34:16 +0530

    sign out button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { useAuthActions } from "@convex-dev/auth/react";
 
 export default function Home() {
-  return <div>Logged In!</div>;
+  const { signOut } = useAuthActions();
+  return (
+    <div>
+      Logged In!
+      <Button onClick={() => signOut()}>Sign out</Button>
+    </div>
+  );
 }
